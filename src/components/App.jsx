1539e9924683c50c981477b1929a427dcdb6c1dd
Rishabh Kanth <72266058+RishabhK200@users.noreply.github.com>
2023-04-25 22:55:29 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Card from "./Card";
+import Contacts from "../contacts";
+
+function createCard(Contacts) {
+  return (
+    <Card
+      name={Contacts.name}
+      img={Contacts.imgURL}
+      phone={Contacts.phone}
+      email={Contacts.email}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Favorite Cricketers Contact List</h1>
+      {Contacts.map(createCard)};
+      {/* <Card
+        name={Contacts[0].name} // attributes
+        img={Contacts[0].imgURL}
+        phone={Contacts[0].phone}
+        email={Contacts[0].email}
+      />
+
+      <Card
+        name={Contacts[1].name}
+        img={Contacts[1].imgURL}
+        phone={Contacts[1].phone}
+        email={Contacts[1].email}
+      />
+
+      <Card
+        name={Contacts[2].name}
+        img={Contacts[2].imgURL}
+        phone={Contacts[2].phone}
+        email={Contacts[2].email}
+      /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/contacts.js
@@ -0,0 +1,27 @@
+const Contacts = [
+  {
+    id: "1",
+    name: "Sachin Tendulkar",
+    imgURL:
+      "https://i.pinimg.com/originals/f7/95/ad/f795adbca930cb8fd25164455d558222.jpg",
+    phone: "+91 8240215420",
+    email: "Sachintendulkar@gmail.com"
+  },
+  {
+    id: "2",
+    name: " Virat Kohli ",
+    imgURL: "https://www.thefamouspeople.com/profiles/images/virat-kohli-7.jpg",
+    phone: "+91 123456780",
+    email: "Viratkohli@gmail.com"
+  },
+  {
+    id: "3",
+    name: "Mahendra Singh Dhoni",
+    imgURL:
+      "https://th.bing.com/th/id/OIP.btD3cdyy2B1KnSatlh9D5gHaE8?pid=ImgDet&rs=1",
+    phone: "+91 8372574104",
+    email: "Mahendrasinghdhoni.com"
+  }
+];
+
+export default Contacts;
